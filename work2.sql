9d3b0363a78b92806ce4f2931132c96921e7d42d
Yang <0>
2019-03-09 21:26:42 +0800

    1

--- work2.sql
+++ work2.sql
@@ -2,26 +2,61 @@ show databases;
 show tables from scott;
 
 select *
-from emp;
+from scott.emp;
 select *
-from dept;
+from scott.dept;
 select *
 from salgrade;
 
 -- 1
-select scott.emp.ENAME, scott.emp.DEPTNO
-from scott.emp
-where job = 'clerk';
+select distinct d.dname, d.deptno
+from scott.dept d,
+     scott.emp e
+where d.DEPTNO = e.DEPTNO;
+
 
 -- 2
-select emp.sal a
-from scott.emp
-where ENAME = 'scott'
-
-union
-
 select *
 from scott.emp
-where ENAME != 'scott' and sal>a;
+where sal > (select scott.emp.SAL from scott.emp where ENAME = 'scott');
+
+-- 3
+select e.ENAME, m.ename
+from scott.emp e
+       left outer join scott.emp m on e.mgr = m.EMPNO;
+
+select e.ename, (select m.ename from scott.emp m where m.EMPNO = e.mgr) ename
+from scott.emp e;
+
+select e.ename, m.ename
+from scott.emp e,
+     scott.emp m
+where e.MGR = m.empno;
+
+
+-- 4
+select e.ename, m.ename
+from scott.emp e
+       inner join scott.emp m on e.mgr = m.EMPNO
+where e.HIREDATE;
+
+select e.ename, m.ename
+from scott.emp e,
+     scott.emp m
+where e.mgr = m.EMPNO
+  and e.HIREDATE;
+
+-- 5
+select e.ENAME, d.DNAME
+from scott.emp e,
+     scott.dept d
+where e.DEPTNO = d.DEPTNO;
+
+-- 6
+select e.ENAME, d.DNAME
+from scott.emp e,
+     scott.dept d
+where job = 'clerk'
+  and e.DEPTNO = d.DEPTNO;
 
 
